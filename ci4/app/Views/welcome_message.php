b0b12329dd30fd170324d2c43aeb4944c6aae09a
Adan Cabildo <alcabildo2@student.apc.edu.ph>
2023-02-13 12:43:13 +0800

    Edited the welcome page

--- ci4/app/Views/welcome_message.php
+++ ci4/app/Views/welcome_message.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>LOCALHOST: Welcome to CodeIgniter 4!</title>
+    <title>APCWEBPROG Welcome to CodeIgniter 4!</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
@@ -222,7 +222,7 @@
 
         <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
-        <h2>CODEIGNITER IN CABILDO LOCALHOST</h2>
+        <h2>CODEIGNITER IN CABILDO APCWEBPROG SERVER</h2>
 
     </div>
 
