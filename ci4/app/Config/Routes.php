14a41bd3ebfddb4e731929f9d31ae6762a82127a
Adan Cabildo <alcabildo2@student.apc.edu.ph>
2023-03-05 20:24:36 +0800

    pls work

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -36,7 +36,7 @@ use App\Controllers\Pages;
 
 $routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'view']);
-$routes->get('news', [News::class, 'index']);
+$routes->get('alcabildo_news', [News::class, 'index']);
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
 /*
